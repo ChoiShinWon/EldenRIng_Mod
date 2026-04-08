d07ad3e6155e7ac90df7efb5bfc30df56ea4ff70
ChoiShinWon <csw57421@gmail.com>
2026-04-08 22:34:25 +0900

    에셋 변경 및 캐릭터 기초 뼈대 구성
    애님 BP 제작중

--- Source/EldenRIng_Mod/Character/EldenCharacter.cpp
+++ Source/EldenRIng_Mod/Character/EldenCharacter.cpp
@@ -1,8 +1,14 @@
 
 #include "EldenRing_Mod/Character/EldenCharacter.h"
+
+#include <rapidjson/document.h>
+
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
 
 
 AEldenCharacter::AEldenCharacter()
@@ -10,13 +16,13 @@ AEldenCharacter::AEldenCharacter()
 	PrimaryActorTick.bCanEverTick = true;
 	
 	// 1. 스프링 암 생성 및 루트 컴포넌트에 부착
-	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(FName("CameraBoom"));
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
 	CameraBoom->TargetArmLength = 400.0f; // 카메라와 캐릭터 사이의 거리
 	CameraBoom->bUsePawnControlRotation = true; // 마우스 움직임에 따라 셀카봉 회전
 	
 	// 2. 카메라 생성 및 스프링 암 끝에 부착
-	FollowCamera = CreateDefaultSubobject<UCameraComponent>(FName("FollowCamera"));
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
 	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); //셀카봉 끝 소켓에 연결
 	FollowCamera->bUsePawnControlRotation = false;
 
@@ -36,6 +42,18 @@ void AEldenCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	// 1. 내 캐릭터를 조종하는 PlayerController를 가져와서 IMC 등록
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			if (DefaultMappingContext)
+			{
+				Subsystem->AddMappingContext(DefaultMappingContext, 0);
+			}
+		}
+	}
+	
 }
 
 // Called every frame
@@ -49,6 +67,50 @@ void AEldenCharacter::Tick(float DeltaTime)
 void AEldenCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	// 2. 입력 신호가 들어올 때 내 클래스의 Move, Look 함수와 묶어주기
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		if (MoveAction)
+		{
+			EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AEldenCharacter::Move);
+		}
+		if (LookAction)
+		{
+			EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AEldenCharacter::Look);
+		}
+	}
 
 }
 
+// 이동 및 회전 로직
+void AEldenCharacter::Move(const FInputActionValue& Value)
+{
+	FVector2D MovementVector = Value.Get<FVector2D>();
+	
+	if (Controller != nullptr)
+	{
+		// 카메라가 바라보는 방향을 가져와서 Pitch, Roll 무시하고 평면(Yaw) 방향만 추출
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw,0);
+		
+		// 그 방향을 기준으로 앞과 오른쪽이 어디인지 절대 벡터로 계산
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		
+		// 캐릭터에 힘 가하기
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
+}
+
+void AEldenCharacter::Look(const FInputActionValue& Value)
+{
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
+	
+	if (Controller != nullptr)
+	{
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
+}
--- Source/EldenRIng_Mod/Character/EldenCharacter.h
+++ Source/EldenRIng_Mod/Character/EldenCharacter.h
@@ -6,6 +6,13 @@
 #include "GameFramework/Character.h"
 #include "EldenCharacter.generated.h"
 
+// 전방 선언
+class USpringArmComponent;
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
 UCLASS()
 class ELDENRING_MOD_API AEldenCharacter : public ACharacter
 {
@@ -17,16 +24,36 @@ public:
 protected:
 	virtual void BeginPlay() override;
 	
+	/*=============================================================================
+	 * Camera
+	 *=============================================================================*/
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
 	class USpringArmComponent* CameraBoom;
 	
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
 	class UCameraComponent* FollowCamera;
+	
+	/*=============================================================================
+	 * Enhanced Input 
+	 *=============================================================================*/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputMappingContext* DefaultMappingContext;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* MoveAction;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* LookAction;
+	
+	// 키보드/마우스에서 신호가 들어왔을 때 실행될 함수들
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+	
+	
+
 
 public:	
 	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
